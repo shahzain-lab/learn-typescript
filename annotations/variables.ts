8cedea0203b72004a33bed9995b56a2b6d249e18
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-10-10 12:37:28 -0700

    learn_basics

---
+++ annotations/variables.ts
@@ -0,0 +1,17 @@
+
+// annotations: Code we add to tell typescript what type of value a variable will
+// refer to. 
+
+// VARIABLE_ANNOTATIONS
+
+const apples: number = 5;
+
+let myName: string = 'zain';
+myName = 'shahzain';
+
+const hasName: boolean = true;
+
+//get ERROR
+// const nothing: undefined = '';
+
+const nothing: undefined = undefined;
---
+++ gettingStartedWithTS/index.js
@@ -0,0 +1,11 @@
+var typescript = {
+    name: 'typescript',
+    compiler: false,
+    rating: 4
+};
+var javascript = {
+    name: 'javascript',
+    compiler: true,
+    rating: 5
+};
+console.log(typescript, javascript);
