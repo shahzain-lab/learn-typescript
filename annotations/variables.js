86ce2924dd73898dac4e3adbe006564e4485426e
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-10-10 13:00:24 -0700

    annotations

---
+++ annotations/variables.js
@@ -0,0 +1,14 @@
+// annotations: Code we add to tell typescript what type of value a variable will
+// refer to. 
+// VARIABLE_ANNOTATIONS
+var apples = 5;
+var myName = 'zain';
+myName = 'shahzain';
+var hasName = true;
+//get ERROR
+// const nothing: undefined = '';
+var nothing = undefined;
+//Built in Objects
+var now = new Date();
+now.getMonth();
+console.log(now);
--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,6 +1,6 @@
 
 // annotations: Code we add to tell typescript what type of value a variable will
-// refer to. 
+// refer to. We can specify the type using :Type after the name of the variable, parameter or property.
 
 // VARIABLE_ANNOTATIONS
 
@@ -15,3 +15,29 @@ const hasName: boolean = true;
 // const nothing: undefined = '';
 
 const nothing: undefined = undefined;
+
+//Built in Objects
+
+let now: Date = new Date();
+now.getMonth();
+
+console.log(now);
+
+// whats about other
+// ARRAY:
+const students: string[] = ['john', 'miller', 'jane'];
+const ages: number[] = [23, 45, 65, 34];
+const areYouThere: boolean[] = [true, false, true, false]
+
+
+// CLASSES:
+class Car {
+
+}
+const car: Car = new Car();
+
+// OBJECTS
+const user: {first: string, age: number} = {
+    first: 'shahzain',
+    age: 16
+}
